cabfa95db8381e28f542f5dd1f7bc2805c636cc4
IrieR <175622981+irierr@users.noreply.github.com>
2022-03-17 18:08:15 +0100

    Path plotting function

--- ShortCutAgents.py
+++ ShortCutAgents.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 
+
 class QLearningAgent(object):
 
     def __init__(self, n_actions, n_states, epsilon, alpha):
@@ -9,29 +10,28 @@ class QLearningAgent(object):
         self.epsilon = epsilon
         self.alpha = alpha
         # TO DO: Add own code
-        self.Q = np.zeros(shape = (self.n_states, self.n_actions))
+        self.Q = np.zeros(shape=(self.n_states, self.n_actions))
         pass
-        
+
     def select_action(self, state):
         # TO DO: Add own code
         p = np.random.uniform()
-        if p <= 1 - self.epsilon: 
-            choices = np.argwhere(self.Q[state]==np.max(self.Q[state])).flatten()
-            a = np.random.choice(choices) # Replace this with correct action selection
+        if p <= 1 - self.epsilon:
+            choices = np.argwhere(self.Q[state] == np.max(self.Q[state])).flatten()
+            a = np.random.choice(choices)  # Replace this with correct action selection
         else:
-            choices = np.argwhere(self.Q[state]!=np.max(self.Q[state])).flatten()
+            choices = np.argwhere(self.Q[state] != np.max(self.Q[state])).flatten()
             if choices.size > 0:
                 a = np.random.choice(choices)
-            else: 
+            else:
                 a = np.random.randint(self.n_actions)
         return a
-        
+
     # TODO: check how to get next_state    
     def update(self, state, next_state, action, reward):
         # TO DO: Add own code
-        
-        self.Q[state,action] += self.alpha*(reward + np.max(self.Q[next_state])-self.Q[state,action])
-        
+        self.Q[state, action] += self.alpha * (reward + np.max(self.Q[next_state]) - self.Q[state, action])
+
 
 class SARSAAgent(object):
 
@@ -41,15 +41,16 @@ class SARSAAgent(object):
         self.epsilon = epsilon
         # TO DO: Add own code
         pass
-        
+
     def select_action(self, state):
         # TO DO: Add own code
-        a = random.choice(range(self.n_actions)) # Replace this with correct action selection
+        a = random.choice(range(self.n_actions))  # Replace this with correct action selection
         return a
-        
+
     def update(self, state, action, reward):
         # TO DO: Add own code
         pass
+
 
 class ExpectedSARSAAgent(object):
 
@@ -59,12 +60,12 @@ class ExpectedSARSAAgent(object):
         self.epsilon = epsilon
         # TO DO: Add own code
         pass
-        
+
     def select_action(self, state):
         # TO DO: Add own code
-        a = random.choice(range(self.n_actions)) # Replace this with correct action selection
+        a = random.choice(range(self.n_actions))  # Replace this with correct action selection
         return a
-        
+
     def update(self, state, action, reward):
         # TO DO: Add own code
         pass
--- ShortCutExperiment.py
+++ ShortCutExperiment.py
@@ -1,16 +1,46 @@
-# Write your experiments in here! You can use the plotting helper functions from the previous assignment if you want.
-
 from ShortCutEnvironment import *
 from ShortCutAgents import *
+from matplotlib import colors
+import matplotlib.pyplot as plt
+
+
+def plot_path(q_values, start_point, end_point, file_name):
+    path = np.zeros(shape=q_values.shape)
+    curr_point = start_point.copy()
+    path[curr_point[0], curr_point[1]] = 2  # Set start point of path
+    while curr_point != end_point:  # Follows path until end point reached
+        if q_values[curr_point[0], curr_point[1]] == 0:  # Up
+            curr_point[0] -= 1
+        elif q_values[curr_point[0], curr_point[1]] == 1:  # Down
+            curr_point[0] += 1
+        elif q_values[curr_point[0], curr_point[1]] == 2:  # Left
+            curr_point[1] -= 1
+        elif q_values[curr_point[0], curr_point[1]] == 3:  # Right
+            curr_point[1] += 1
+        path[curr_point[0], curr_point[1]] = 1  # Update path
+    path[curr_point[0], curr_point[1]] = 3  # Set end point of path
+
+    fig, ax = plt.subplots()
+    cmap = colors.ListedColormap(['white', 'yellow', 'blue', 'green'])  # Set colors
+    # Show grid
+    plt.hlines(y=np.arange(0, 12)+0.5, xmin=np.full(12, 0)-0.5, xmax=np.full(12, 12)-0.5, color='black')
+    plt.vlines(x=np.arange(0, 12)+0.5, ymin=np.full(12, 0)-0.5, ymax=np.full(12, 12)-0.5, color='black')
+    # Remove axis
+    ax.get_xaxis().set_ticks([])
+    ax.get_yaxis().set_ticks([])
+
+    ax.matshow(path, cmap=cmap)
+    plt.savefig(file_name)
+
 
 def main():
-    SCE = ShortcutEnvironment() 
+    SCE = ShortcutEnvironment()
     n_states = SCE.state_size()
     n_actions = len(SCE.possible_actions())
     epsilon = 0.1
     alpha = 0.1
     QLA = QLearningAgent(n_actions, n_states, epsilon, alpha)
-    for i in range(1000):
+    for i in range(10000):
         current_state = SCE.state()
         while not SCE.done():
             a = QLA.select_action(current_state)
@@ -18,13 +48,11 @@ def main():
             next_state = SCE.state()
             QLA.update(current_state, next_state, a, r)
             current_state = next_state
-        SCE.reset()    
-    print(np.argmax(QLA.Q,axis=1).reshape((12,12)))
+        SCE.reset()
+
+    q_values_2d = np.argmax(QLA.Q, axis=1).reshape((12, 12))
+    plot_path(q_values_2d, [9, 2], [8, 8], "path.png")
 
 
-    
-        
 if __name__ == '__main__':
     main()
-
-

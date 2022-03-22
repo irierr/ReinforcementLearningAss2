0323b39ffa7e8f107d2b816f35c5d880f72614a7
Pablo Ubilla <48734361+pabloubilla@users.noreply.github.com>
2022-03-22 11:09:29 +0100

    SARSA agent

--- ShortCutAgents.py
+++ ShortCutAgents.py
@@ -1,6 +1,18 @@
 import random
 import numpy as np
 
+def choose_greedy(X,e):
+    p = np.random.uniform()
+    if p <= 1 - e:
+        choices = np.argwhere(X == np.max(X)).flatten()
+        a = np.random.choice(choices)  # Replace this with correct action selection
+    else:
+        choices = np.argwhere(X != np.max(X)).flatten()
+        if choices.size > 0:
+            a = np.random.choice(choices)
+        else:
+            a = np.random.randint(X.size)
+    return a 
 
 class QLearningAgent(object):
 
@@ -15,17 +27,17 @@ class QLearningAgent(object):
 
     def select_action(self, state):
         # TO DO: Add own code
-        p = np.random.uniform()
-        if p <= 1 - self.epsilon:
-            choices = np.argwhere(self.Q[state] == np.max(self.Q[state])).flatten()
-            a = np.random.choice(choices)  # Replace this with correct action selection
-        else:
-            choices = np.argwhere(self.Q[state] != np.max(self.Q[state])).flatten()
-            if choices.size > 0:
-                a = np.random.choice(choices)
-            else:
-                a = np.random.randint(self.n_actions)
-        return a
+        # p = np.random.uniform()
+        # if p <= 1 - self.epsilon:
+        #     choices = np.argwhere(self.Q[state] == np.max(self.Q[state])).flatten()
+        #     a = np.random.choice(choices)  # Replace this with correct action selection
+        # else:
+        #     choices = np.argwhere(self.Q[state] != np.max(self.Q[state])).flatten()
+        #     if choices.size > 0:
+        #         a = np.random.choice(choices)
+        #     else:
+        #         a = np.random.randint(self.n_actions)
+        return choose_greedy(self.Q[state],self.epsilon)
 
     # TODO: check how to get next_state    
     def update(self, state, next_state, action, reward):
@@ -35,21 +47,23 @@ class QLearningAgent(object):
 
 class SARSAAgent(object):
 
-    def __init__(self, n_actions, n_states, epsilon):
+    def __init__(self, n_actions, n_states, epsilon, alpha):
         self.n_actions = n_actions
         self.n_states = n_states
         self.epsilon = epsilon
+        self.alpha = alpha
         # TO DO: Add own code
-        pass
+        self.Q = np.zeros(shape=(self.n_states, self.n_actions))
+
 
     def select_action(self, state):
         # TO DO: Add own code
-        a = random.choice(range(self.n_actions))  # Replace this with correct action selection
-        return a
+        return choose_greedy(self.Q[state],self.epsilon)
 
-    def update(self, state, action, reward):
-        # TO DO: Add own code
-        pass
+
+    def update(self, state, next_state, action, reward):
+        next_action = choose_greedy(self.Q[next_state],self.epsilon)
+        self.Q[state,action] += self.alpha*(reward + self.Q[next_state, next_action] - self.Q[state,action])
 
 
 class ExpectedSARSAAgent(object):

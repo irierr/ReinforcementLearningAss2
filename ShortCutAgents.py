b11b537f910fd9a706c2a90e43db53aa8a309f21
IrieR <175622981+irierr@users.noreply.github.com>
2022-03-24 16:26:44 +0100

    added final experiment code and 2 path plot

--- ShortCutAgents.py
+++ ShortCutAgents.py
@@ -1,4 +1,3 @@
-#%%
 import numpy as np
 
 
@@ -27,7 +26,6 @@ class Agent(object):
         return a
 
 
-
 class QLearningAgent(Agent):
 
     def __init__(self, n_actions, n_states, epsilon, alpha):
@@ -44,7 +42,7 @@ class SARSAAgent(Agent):
 
     def update(self, state, next_state, action, reward):
         next_action = super().choose_greedy(self.Q[next_state])
-        self.Q[state, action] += self.alpha*(reward + self.Q[next_state, next_action] - self.Q[state, action])
+        self.Q[state, action] += self.alpha * (reward + self.Q[next_state, next_action] - self.Q[state, action])
 
 
 class ExpectedSARSAAgent(Agent):
@@ -56,17 +54,15 @@ class ExpectedSARSAAgent(Agent):
         probs_policy = np.zeros(self.n_actions)
         args_max = np.argwhere(Q == np.max(Q)).flatten()
         n_max = len(args_max)
-        if n_max<self.n_actions:
-            trues_max = np.full(self.n_actions,False,dtype=bool)
+        if n_max < self.n_actions:
+            trues_max = np.full(self.n_actions, False, dtype=bool)
             trues_max[args_max] = True
-            probs_policy[trues_max] = (1-self.epsilon)/n_max
-            probs_policy[~trues_max] = self.epsilon/(self.n_actions-n_max)
+            probs_policy[trues_max] = (1 - self.epsilon) / n_max
+            probs_policy[~trues_max] = self.epsilon / (self.n_actions - n_max)
         else:
-            probs_policy[:] = 1/self.n_actions
-        return np.sum(Q*probs_policy)
+            probs_policy[:] = 1 / self.n_actions
+        return np.sum(Q * probs_policy)
 
     def update(self, state, next_state, action, reward):
-        self.Q[state, action] += self.alpha*(reward + self.expected_epsilon_greedy(self.Q[next_state]) - self.Q[state, action])
-     
-
-# %%
+        self.Q[state, action] += self.alpha * (
+                    reward + self.expected_epsilon_greedy(self.Q[next_state]) - self.Q[state, action])
--- ShortCutExperiment.py
+++ ShortCutExperiment.py
@@ -6,21 +6,22 @@ from ShortCutAgents import *
 from ShortCutEnvironment import *
 
 
-def plot_path(q_values, start_point, end_point, file_name):
+def plot_path(q_values, start_points, end_point, file_name):
     path = np.zeros(shape=q_values.shape)
-    curr_point = start_point.copy()
-    path[curr_point[0], curr_point[1]] = 2  # Set start point of path
-    while curr_point != end_point:  # Follows path until end point reached
-        if q_values[curr_point[0], curr_point[1]] == 0:  # Up
-            curr_point[0] -= 1
-        elif q_values[curr_point[0], curr_point[1]] == 1:  # Down
-            curr_point[0] += 1
-        elif q_values[curr_point[0], curr_point[1]] == 2:  # Left
-            curr_point[1] -= 1
-        elif q_values[curr_point[0], curr_point[1]] == 3:  # Right
-            curr_point[1] += 1
-        path[curr_point[0], curr_point[1]] = 1  # Update path
-    path[curr_point[0], curr_point[1]] = 3  # Set end point of path
+    for start_point in start_points:
+        curr_point = start_point.copy()
+        path[curr_point[0], curr_point[1]] = 2  # Set start point of path
+        while curr_point != end_point:  # Follows path until end point reached
+            if q_values[curr_point[0], curr_point[1]] == 0:  # Up
+                curr_point[0] -= 1
+            elif q_values[curr_point[0], curr_point[1]] == 1:  # Down
+                curr_point[0] += 1
+            elif q_values[curr_point[0], curr_point[1]] == 2:  # Left
+                curr_point[1] -= 1
+            elif q_values[curr_point[0], curr_point[1]] == 3:  # Right
+                curr_point[1] += 1
+            path[curr_point[0], curr_point[1]] = 1  # Update path
+    path[end_point[0], end_point[1]] = 3  # Set end point of path
 
     fig, ax = plt.subplots()
     # Show grid
@@ -64,67 +65,77 @@ def run_episodes(n_episodes, environment, agent):
     return rewards
 
 
-def run_repetitions(n_rep, n_episodes, epsilon, alpha, method, environment):
+def run_repetitions(n_rep, n_episodes, epsilon, alpha, agent_method, environment):
     rewards = np.zeros(shape=(n_rep, n_episodes))
-
+    env = environment()
+    n_states = env.state_size()
+    n_actions = len(env.possible_actions())
     for i in range(n_rep):
-        env = environment()
-        n_states = env.state_size()
-        n_actions = len(env.possible_actions())   
-        agent = method(n_actions, n_states, epsilon, alpha)
+        agent = agent_method(n_actions, n_states, epsilon, alpha)
         rewards[i] = run_episodes(n_episodes, env, agent)
+        env.reset()
     return np.mean(rewards, axis=0)
 
 
-def run_repetitions_parallel(n_rep, n_episodes, epsilon, alpha, method, environment):
-    results = []
+def run_repetitions_parallel(n_rep, n_episodes, epsilon, alpha, agent_method, environment):
+    async_results = []
     pool = Pool(processes=cpu_count())
     for i in range(n_rep):
         env = environment()
-        n_states = env.state_size()
-        n_actions = len(env.possible_actions())
-        agent = method(n_actions, n_states, epsilon, alpha)
-        results.append(pool.apply_async(run_episodes, args=(n_episodes, env, agent)))
+        agent = agent_method(len(env.possible_actions()), env.state_size(), epsilon, alpha)
+        async_results.append(pool.apply_async(run_episodes, args=(n_episodes, env, agent)))
     pool.close()
     pool.join()
-    return np.mean([r.get() for r in results], axis=0)
+    return np.mean([r.get() for r in async_results], axis=0)
 
 
-def run_experiment(n_rep, n_episodes, epsilon, alpha_values, method, environment, parallel=True):
-    results = np.zeros(shape=(len(alpha_values), n_episodes))
+def run_experiment(n_rep, n_episodes, epsilon, alpha_values, agent, environment, parallel=True):
+    means = np.zeros(shape=(len(alpha_values), n_episodes))
     for i, a in enumerate(alpha_values):
         if parallel:
-            results[i] = run_repetitions_parallel(n_rep, n_episodes, epsilon, a, method, environment)
+            means[i] = run_repetitions_parallel(n_rep, n_episodes, epsilon, a, agent, environment)
         else:
-            results[i] = run_repetitions(n_rep, n_episodes, epsilon, a, method, environment)
+            means[i] = run_repetitions(n_rep, n_episodes, epsilon, a, agent, environment)
         print(f"alpha value {a} done!")
-    return results
+    return means
 
 
 def main():
     epsilon = 0.1
-    alphas = [0.5, 0.9]#[0.01, 0.1, 0.5, 0.9]
-    n_episodes = 10
-    n_reps = 100 
-    methods = [QLearningAgent, SARSAAgent, ExpectedSARSAAgent]
-    methods_names = ['Q-Learning', 'SARSA', 'Expected_SARSA']
+    # alphas = [0.01, 0.1, 0.5, 0.9]
+    # n_episodes = 1000
+    # n_reps = 100
+    # agents = [QLearningAgent, SARSAAgent, ExpectedSARSAAgent]
+    # agents_names = ['Q-Learning', 'SARSA', 'Expected_SARSA']
+    # rewards = {}
+    start = time.time()
+    # for a in range(len(agents)):
+    #     an = agents_names[a]
+    #     print(an)
+    #     rewards[an] = run_experiment(n_reps, n_episodes, epsilon, alphas, agents[a], ShortcutEnvironment)
+    #     plot_reward_graph(rewards[an], alphas, f"{an}_rewards")
+    #     np.save(f"reward_arrays/{an}", rewards[an])
+
+    alpha = 0.1
+    n_episodes = 10000
+    agents = [QLearningAgent, SARSAAgent, ExpectedSARSAAgent]
+    agents_names = ['Q-Learning', 'SARSA', 'Expected_SARSA']
     environments = [ShortcutEnvironment, WindyShortcutEnvironment]
     environment_names = ['Shortcut', 'Windy_Shortcut']
-    Rewards = {}
-    start = time.time()
-    for m in range(len(methods)):
+    for a in range(len(agents)):
         for e in range(len(environments)):
-            mn, en = methods_names[m],environment_names[e]
-            print(mn,en)
-            Rewards[mn,en] = run_experiment(n_reps, n_episodes, epsilon, alphas, methods[m], environments[e], parallel=True)
-            plot_reward_graph(Rewards[mn,en], alphas, mn+'_'+en)
+            if a == ExpectedSARSAAgent and e == WindyShortcutEnvironment:
+                continue
+            an, en = agents_names[a], environment_names[e]
+            print(an, en)
+            env = environments[e]()
+            agent = agents[a](len(env.possible_actions()), env.state_size(), epsilon, alpha)
+            _ = run_episodes(n_episodes, env, agent)
+            q_values_2d = np.argmax(agent.Q, axis=1).reshape((12, 12))
+            plot_path(q_values_2d, [[9, 2], [2, 2]], [8, 8], "q_path")
+            np.save(f"Q_Arrays/{an}_{en}", q_values_2d)
     end = time.time()
     print(f"Whole experiment done in: {end - start}")
-
-    
-
-    # q_values_2d = np.argmax(QLA_Q, axis=1).reshape((12, 12))
-    # plot_path(q_values_2d, [9, 2], [8, 8], "q_path")
 
 
 if __name__ == '__main__':
